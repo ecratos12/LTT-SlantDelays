c009d87e37472d310776f4619de296a1b5c571ad
ecratos12 <ecratos12@gmail.com>
2024-05-01 15:36:07 +0300

    Redone compilation procedure

--- compile.bash
+++ compile.bash
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-flags="-fcheck=all -fbacktrace -fmax-errors=10"
-IXX="-I${ECCODES_INSTALL_ROOT}/include"
-LXX="-L${ECCODES_INSTALL_ROOT}/lib"
-source_folder="$1"
-exe="$2"
 
-test -d binaries || mkdir -p binaries/
-cd binaries/
+# to install ecCodes, get help here:
+# https://confluence.ecmwf.int/download/attachments/73011814/eccodes_bufr_deploy.pdf?api=v2
+export ECCODES_INSTALL_ROOT=$YOUR_ECCODES_INSTALLATION_DIR
 
-gfortran -g $flags -o $exe ../$source_folder/*.f90 $IXX $LXX -Wl,-rpath=${ECCODES_INSTALL_ROOT}/lib -leccodes_f90
-cp $exe ../$exe
+mkdir bin && cd bin
+make -f ../sources/ltt-modular/Makefile clean
+make -f ../sources/ltt-modular/Makefile
+cp *.exe ..
